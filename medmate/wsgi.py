b47d6fdabe151fae2e217375112c6f8d0b41c1a5
Sobhan Ghosh <sobhanghosh.jsr@gmail.com>
2025-01-29 23:47:24 +0530

    remove errors

--- medmate/wsgi.py
+++ medmate/wsgi.py
@@ -6,11 +6,20 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
-
 import os
+import sys
+
+
+path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if path not in sys.path:
+    sys.path.append(path)
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'medmate.settings')
 
-application = get_wsgi_application()
+try:
+    application = get_wsgi_application()
+except Exception as e:
+    print(f"Error loading WSGI application: {e}")
+    raise
