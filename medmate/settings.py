35c45ccb03df940b26d0f7fcde2c28c10a338fb9
Sobhan Ghosh <sobhanghosh.jsr@gmail.com>
2025-02-13 02:05:06 +0530

    fix errors for deplyment

--- medmate/settings.py
+++ medmate/settings.py
@@ -15,6 +15,10 @@ from decouple import config
 import firebase_admin
 from firebase_admin import credentials
 from datetime import timedelta
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
