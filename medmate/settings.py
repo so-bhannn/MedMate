2498dbe7146481d2caa47ae78f39b3cfb479d15e
Sobhan Ghosh <sobhanghosh.jsr@gmail.com>
2025-02-17 00:01:15 +0530

    fixes for deployments

--- medmate/settings.py
+++ medmate/settings.py
@@ -40,9 +40,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DJANGO_DEBUG')
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['medmate-production.up.railway.app','https://medmate-production.up.railway.app']
 
 
 # Application definition
--- medmate/urls.py
+++ medmate/urls.py
@@ -20,7 +20,6 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import *
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path("chat/", include('chatbot.urls')),
     path("api/token", TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path("api/token/refresh", TokenRefreshView.as_view(), name='token_refresh'),
