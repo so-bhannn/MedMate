b31562ea5e48f698f3f92e063fc018edc52c2393
Sobhan Ghosh <sobhanghosh.jsr@gmail.com>
2025-03-23 23:14:28 +0530

    update food recipe endpoint

--- food/urls.py
+++ food/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.random_food_recipe, name='random_recipe')
+    path('random_food/', views.random_food_recipe, name='random_recipe')
 ]
